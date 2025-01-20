6aed9688cd55152f7ce60d385352e2305eb30781
lwmacct <lwmacct@icloud.com>
2025-01-21 02:09:52 +0800

    release v0.1.1

--- app/start/cmd.go
+++ app/start/cmd.go
@@ -17,6 +17,6 @@ func Cmd() *m_cmd.Ts {
 
 func run(cmd *cobra.Command, args []string) {
 	_ = map[string]any{"cmd": cmd, "args": args}
-	m_log.Info(m_log.H{"msg": "app.Flag", "data": app.Flag})
+	m_log.Info(m_log.H{"msg": "hello world"})
 
 }
